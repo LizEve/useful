562f7432e1db35d3cd66a83f52e2d7a9c4996ff4
Genevieve Mount <gmount1@lsu.edu>
2015-04-01 17:54:07 -0500

    updated file

--- iter_rmfiles.sh
+++ iter_rmfiles.sh
@@ -2,10 +2,22 @@
 #removing a file from multiple folders
 #this is finding all files that end with .nex use the basename to find folders with the basename
 #then move into that folder and remove the file "1"
-for f in *.nex; 
+for f in squam*; 
+do
+base=`basename $f squam_`
+echo $base
+cd $base
+rm squam*
+cd ../
+done
+
+
+
+for f in squam*.nex; 
 do
 base=`basename $f .nex`
 cd $base
-rm 1
+echo $base
+rm *.conf
 cd ../
 done
